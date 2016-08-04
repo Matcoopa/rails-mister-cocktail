fea5965ec0004eb25b07f2209245fc2efd305a79
Matthieu Grebot <matcoopa@gmail.com>
2016-08-04 15:39:10 +0100

    Routing done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Ingredient.destroy_all
+ingredients_attributes = [
+  { name:         "lemon" },
+  { name:         "ice" },
+  { name:         "mint leave" }
+]
+ingredients_attributes.each { |params| Ingredient.create!(params) }
