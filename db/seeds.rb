02b19aeca7b3ad3c20ad6210512b4bd40c986935
Matthieu Grebot <matcoopa@gmail.com>
2016-08-04 16:41:57 +0100

    Cocktails view and ingredients details are ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Ingredient.destroy_all
 ingredients_attributes = [
   { name:         "lemon" },
   { name:         "ice" },
   { name:         "mint leave" }
 ]
-ingredients_attributes.each { |params| Ingredient.create!(params) }
+ingredients_attributes.each { |params| Ingredient.create(params) }
+
+cocktails_attributes = [
+  { name:         "Genuine Lemon" },
+  { name:         "Charbet" },
+  { name:         "Iced tea" }
+]
+cocktails_attributes.each { |params| Cocktail.create(params) }
