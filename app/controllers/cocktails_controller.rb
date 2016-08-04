6af5bc8838aa47aedeaa5814844b8af8c3de53ae
Matthieu Grebot <matcoopa@gmail.com>
2016-08-04 23:02:22 +0100

    Models cocktails, doses et ingredients + lookup sur ingredients. style missing

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -7,6 +7,20 @@ class CocktailsController < ApplicationController
   end
 
   def show
+    @dose = Dose.new
+  end
+
+
+  def new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+        redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
 private
@@ -15,4 +29,7 @@ private
     @cocktail = Cocktail.find(params[:id])
   end
 
+  def cocktail_params
+    params.require(:cocktails).permit(:name)
+  end
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,5 @@
 class Ingredient < ActiveRecord::Base
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   has_many :doses
+  has_many :cocktails, through: :doses
 end
